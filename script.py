5f610adf0502b2269f4df0ac13f5f2d2794b02eb
Quentin Foux <quen.foux@laposte.net>
2021-03-17 17:53:22 +0100

    Update2

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-import biketrauma
+import biketrauma_FOUX
 
-df = biketrauma.Load_db().save_as_df()
-biketrauma.plot_location(biketrauma.get_accident(df))
+df = biketrauma_FOUX.Load_db().save_as_df()
+biketrauma_FOUX.plot_location(biketrauma_FOUX.get_accident(df))
--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
-from biketrauma import __version__ as current_version
+from biketrauma_FOUX import __version__ as current_version
 
 setup(
-  name='biketrauma',
+  name='biketrauma_FOUX',
   version=current_version,
   description='Visualization of a bicycle accident db',
   url='http://github.com/fawerewaf.git',
   author='fawerewaf',
   author_email='quen.foux@laposte.net',
   license='MIT',
-  packages=['biketrauma','biketrauma.io', 'biketrauma.preprocess', 'biketrauma.vis'],
+  packages=['biketrauma_FOUX','biketrauma_FOUX.io', 'biketrauma_FOUX.preprocess', 'biketrauma_FOUX.vis'],
   zip_safe=False
 )
 
