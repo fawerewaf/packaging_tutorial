3f9c5bea33ace634958f3c9dc018eacadfaff923
Quentin Foux <quen.foux@laposte.net>
2021-03-17 17:16:08 +0100

    Update

--- biketrauma/vis/plot_location.py
+++ biketrauma/vis/plot_location.py
@@ -5,7 +5,9 @@ import pygal                                                       # First impor
 def plot_location(gd):
   fr_chart = pygal.maps.fr.Departments(human_readable=True)
   fr_chart.title = 'Accident by region'
+
   fr_chart.add('Accidents', gd.to_dict())
 
   fr_chart.render_in_browser()
-  # fr_chart.render_to_file('./chart.svg')  # Write the chart in the specified fileS
+  # fr_chart.render_to_file('./chart.svg')  # Write the chart in the specified file
+
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from biketrauma import __version__ as current_version
+
+setup(
+  name='biketrauma',
+  version=current_version,
+  description='Visualization of a bicycle accident db',
+  url='http://github.com/fawerewaf.git',
+  author='fawerewaf',
+  author_email='quen.foux@laposte.net',
+  license='MIT',
+  packages=['biketrauma','biketrauma.io', 'biketrauma.preprocess', 'biketrauma.vis'],
+  zip_safe=False
+)
+
